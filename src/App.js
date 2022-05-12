099fa37ece7d5f38801cb5d9ed6506683b01fbba
MauricioAMartinez <mauricio.m@montechelo.com.co>
2022-05-12 18:55:56 -0500

    state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from "react";
 import "./App.css";
 
-function Helloworld(props) {
-  const { message } = props;
-  return <div id="hello">Hello {message} </div>;
+/* function Helloworld(props) {
+  const {message} = props
+  return (<div id="hello">Hello {message} </div>)
+} */
+
+class Helloworld extends React.Component {
+  state = {
+    show: true,
+  };
+
+  toggleShow = () => {
+    this.setState({ show: !this.state.show });
+  };
+
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          Hello {this.props.message}
+          <button onClick={this.toggleShow}>Togle Show</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>No hay elementos</h1>
+          <button onClick={this.toggleShow}>Togle Show</button>
+        </div>
+      );
+    }
+  }
 }
 
 function App() {
