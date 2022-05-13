6608e22701a9a06822e406347b7718c4d266d0cb
MauricioAMartinez <mauricio.m@montechelo.com.co>
2022-05-12 21:19:59 -0500

    Styles And Objects

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,21 +1,43 @@
 import React, { Component } from "react";
-
+import './Task.css'
 class Task extends Component {
+
+  StyleCompleted(done){
+    console.log(done)
+    return {
+      fontSize:'20px',
+      color: done ? 'gray' : 'black',
+      textDecoration: done ? 'line-through' : 'none'
+    }
+  }
+
+
   render() {
 
     const {task} =  this.props
-
+    //const redColor = {background :'red'}
     return (
-      <div>
+      <p style={this.StyleCompleted(task.done)} >
         {task.tittle} - 
         {task.description}-
         {task.done} - 
         {task.id}
         <input type="checkbox" />
-        <button>x</button>
-      </div>
+        <button style={btnDelete} >x</button>
+      </p>
     );
   }
+}
+
+
+const btnDelete = {
+  fontSize: '18px',
+  background :'red',
+  color:'#fff',
+  border: 'none',
+  padding: '10px 15px',
+  borderRadius: '50%',
+  cursor:'pointer'
 }
 
 export default Task;
